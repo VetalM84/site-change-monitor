6bee6fa0dfbb2b3e58b3ab42da4a92ea4e48e5cc
Vitalii Mytenko <vetalmit@gmail.com>
2023-04-12 13:24:51 +0300

    Hardcode directory path for project and output files.

--- main.py
+++ main.py
@@ -38,8 +38,9 @@ class RequestHandler:
 class JsonHandler:
     """Class to work with json files."""
 
-    def __init__(self, file_dir: str, file_name: str):
-        self._file_dir = file_dir
+    _file_dir: str = None
+
+    def __init__(self, file_name: str):
         self._file_name = file_name
         self.full_path = self.set_full_path()
 
@@ -63,10 +64,10 @@ class JsonHandler:
 class JsonProjectConfig(JsonHandler):
     """Class to work with json files with project config."""
 
-    def __init__(
-        self, file_dir: str = "projects_configs", file_name: str = "new_project.json"
-    ):
-        super().__init__(file_dir, file_name)
+    _file_dir: str = "projects_configs"
+
+    def __init__(self, file_name: str):
+        super().__init__(file_name)
         self._project_root = self.get_project_config()
         self.home_url = self._project_root["home_url"]
         self.paginator_pattern = self._project_root["paginator_pattern"]
@@ -83,21 +84,23 @@ class JsonProjectConfig(JsonHandler):
         """Get project config."""
         return self.read_json_file()
 
-
-def find_all_project_files(file_dir) -> list:
-    """Find all json project config files in a directory."""
-    dir_path = Path(file_dir)
-    files = os.listdir(dir_path)
-    return [f for f in files if f.endswith(".json")]
+    @staticmethod
+    def find_all_project_files(file_dir: str = _file_dir) -> list:
+        """Find all json project config files in a directory."""
+        dir_path = Path(file_dir)
+        files = os.listdir(dir_path)
+        return [f for f in files if f.endswith(".json")]
 
 
 class JsonItems(JsonHandler):
     """Class to work with json files with items."""
 
-    def __init__(self, file_dir: str, file_name: str):
-        super().__init__(file_dir, file_name)
+    _file_dir: str = "items_list_output"
+
+    def __init__(self, file_name: str):
+        super().__init__(file_name)
         # Check if the directory exists, and create it if it doesn't.
-        Path(file_dir).mkdir(parents=True, exist_ok=True)
+        Path(self._file_dir).mkdir(parents=True, exist_ok=True)
 
     def read_json_file(self):
         """Read json items file, create it if it is not exists."""
@@ -119,44 +122,6 @@ class JsonItems(JsonHandler):
         existing_data.update(data)
         with open(self.full_path, "w", encoding="utf-8") as f:
             json.dump(existing_data, f, indent=4, ensure_ascii=False)
-
-
-def save_html_file(file_name, response, counter=0) -> None:
-    """Save html file."""
-    with open(f"data/{file_name}_{counter}.html", "w", encoding="utf-8") as f:
-        f.write(response.text)
-
-
-def read_html_file(file_name: str) -> str:
-    """Read html file."""
-    with open(f"data/{file_name}.html", "r", encoding="utf-8") as f:
-        src = f.read()
-    return src
-
-
-# def scrap_links(source, project_settings: JsonProjectConfig) -> list:
-#     """Scrap links."""
-#     links = []
-#     soup = BeautifulSoup(source, "lxml")
-#
-#     for link_tag in soup.findAll(
-#         project_settings.items_container["tag"],
-#         {"class": project_settings.items_container["class"]},
-#     ):
-#         links.append(project_settings.home_url + link_tag.get("href"))
-#
-#     return links
-
-
-# def scrap_links(source, link_css_class: str, domain: str) -> list:
-#     """Scrap links."""
-#     links = []
-#     soup = BeautifulSoup(source, "lxml")
-#
-#     for link_tag in soup.findAll("a", {"class": link_css_class}):
-#         links.append(domain + link_tag.get("href"))
-#
-#     return links
 
 
 def scrap_single_item(source, project_settings: JsonProjectConfig) -> Tuple[str, dict]:
@@ -246,13 +211,9 @@ def main():
     changed_or_new_items: list[dict] = []
     request = RequestHandler()
 
-    for project in find_all_project_files("projects_configs"):
-        json_project_config = JsonProjectConfig(
-            file_dir="projects_configs", file_name=project
-        )
-        json_items_list = JsonItems(
-            file_dir="items_list_output", file_name="output_" + project
-        )
+    for project in JsonProjectConfig.find_all_project_files():
+        json_project_config = JsonProjectConfig(file_name=project)
+        json_items_list = JsonItems(file_name="output_" + project)
 
         # iterate over pagination
         for page_index in range(1, json_project_config.pagination_count + 1):
