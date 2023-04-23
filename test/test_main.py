94044c2e749404f6ef4d348410881e72480e7839
Vitalii Mytenko <vetalmit@gmail.com>
2023-04-23 19:08:09 +0300

    Add test_get_all_items_to_check_single_page

--- test/test_main.py
+++ test/test_main.py
@@ -25,21 +25,22 @@ def test_get_all_items_to_check(items_container_html_source, mock_project_object
         items_container_html_source, mock_project_object, 0
     )
     assert len(all_items) == 24
-
     # Test case 2: Get all items when container is specified with selector
     all_items = get_all_items_to_check(
         items_container_html_source, mock_project_object, 0
     )
     assert len(all_items) == 24
-
-    # Test case 3: Get single item when container is specified with single_url
-    # all_items = get_all_items_to_check(
-    #     items_container_html_source, mock_project_object, 1
-    # )
-    # assert len(all_items) == 1
-
     # Test case 4: Return empty list when no items are found in the container
     all_items = get_all_items_to_check(
         items_container_html_source, mock_project_object, 0
     )
     assert len(all_items) == 0
+
+
+def test_get_all_items_to_check_single_page(single_page_html_source, mock_project_object_single_page):
+    """Test get_all_items_to_check function with single_url."""
+    all_items = get_all_items_to_check(
+        single_page_html_source, mock_project_object_single_page, 1
+    )
+    assert len(all_items) == 1
+
